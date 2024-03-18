a3dfeb129bfc2cea3275328c4c07b3fac8c545d9
liuzhuozhuo <liugeorge18@gmail.com>
2024-03-18 19:00:27 +0100

    modify rand_num.c

--- funciones/rand_num.c
+++ funciones/rand_num.c
@@ -1,26 +1,21 @@
-     #include <stdio.h>
-     #include <gsl/gsl_rng.h>
-     
-     int
-     main (void)
-     {
-       const gsl_rng_type * T;
-       gsl_rng * r;
-     
-       int i, n = 10;
-     
-       gsl_rng_env_setup();
-     
-       T = gsl_rng_default;
-       r = gsl_rng_alloc (T);
-     
-       for (i = 0; i < n; i++) 
-         {
-           double u = gsl_rng_uniform (r);
-           printf ("%.5f\n", u);
-         }
-     
-       gsl_rng_free (r);
-     
-       return 0;
-     }
+#include <iostream>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_rng.h>
+#include <sys/time.h>
+
+float keithRandom() {
+    // Random number function based on the GNU Scientific Library
+    // Returns a random float between 0 and 1, exclusive; e.g., (0,1)
+    const gsl_rng_type * T;
+    gsl_rng * r;
+    gsl_rng_env_setup();
+    struct timeval tv; // Seed generation based on time
+    gettimeofday(&tv,0);
+    unsigned long mySeed = tv.tv_sec + tv.tv_usec;
+    T = gsl_rng_default; // Generator setup
+    r = gsl_rng_alloc (T);
+    gsl_rng_set(r, mySeed);
+    double u = gsl_rng_uniform(r); // Generate it!
+    gsl_rng_free (r);
+    return (float)u;
+}
