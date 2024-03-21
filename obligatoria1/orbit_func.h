1f482638e16ca8eb802f295ad4265013d5acdfed
liuzhuozhuo <liugeorge18@gmail.com>
2024-03-21 12:03:31 +0100

    init files

---
+++ obligatoria1/orbit_func.h
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Update the acceleration vector, 
+//r: matrix (pointer) with the position of all the planets in 3d
+//a: matrix (pointer) with the acceleration of all the planets in 3d
+//m: vector (pointer) with the modified mass of the planets
+//N is the total number of planets
+void acceleration (double* r, double* a, double* m, int N) {
+    int i, j, k;
+    double acc; //Dummy variable to sum all the acceleration for a planet in one component
+    double R[N][N][3]; 
+    double R_mod[N][N]; //Matrix NxN with the module of the distance between planets
+
+    //Calculate the distance vector between planets. 
+    for(i = 0; i < N-1; i++){
+        for (j = i; j < N-1; j++){
+            R_mod[i][j]= 0;
+            for (k = 0; k < 3; k++){
+                R[i][j][k] = *(r+i*N+k) - *(r+j*N+k); 
+                R_mod[i][j] += pow(R[i][j][k], 2);
+            }
+        }
+    }
+    //Calculate the acceleration
+    //iterate in the planets
+     for(i = 0; i < N; i++){
+        //iterate in the components
+        for (k = 0; k < 3; k++){
+            //iterate between planets
+            acc = 0;
+            for (j = 0; j < N; j++){
+                if(j>i){
+                    acc += (*(m+j))*R[i][j][k]/pow(R_mod[i][j], 3./2.);
+                }
+                if(j<i){
+                    acc += -(*(m+j))*R[j][i][k]/pow(R_mod[j][i], 3./2.);
+                }
+            } 
+            *(a+i*N+k) = acc;
+        } 
+    }    
+}
+
+//Function that calculate the next set of position and velocity using the verlet algorithms
+void verlet_algorithm(double* r, double* v ,double* a, double* m, int N, double h){
+    int i, j, k;
+    double w[N][3];
+
+    //Calculate the new positions
+    for (i = 0; i < N; i++){
+        for (k = 0; k < 3; k++){
+            *(r+i*N+k) += h*(*(v+i*N+k)) + (pow(h, 2)/2.) * (*(a+i*N+k));
+            w[N][k] = *(v+i*N+k) + (h/2.)*(*(a+i*N+k));
+        }
+    }
+   //Using the acceleration function calculate the net acceleration for each of the planets
+    acceleration(r, a, m, N);
+
+    //Calculate the new velocity of the planets
+    for (i = 0; i < N; i++){
+        for (k = 0; k < 3; k++){
+            *(v+i*N+k) = w[N][k] + (h/2.)*(*(a+i*N+k));
+        }
+    }
+}
---
+++ obligatoria1/planet_orbits.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "orbit_func.h"
+
+int main(){
+        // Define t of the simulation, and the step h
+    double t = 10.;
+    double h= 0.1;
+
+    // Rescale the magnitudes and define some constants
+    double G = 6.67408e-11; //N m2/kg2, Gravitational constant
+    double c = 1.4966e11; //m, astronomical units
+    double M_s = 1.99e30; //kg, solar mass
+    int N = 3; //Number of planets
+
+    int i, j, k; // Used for the loop
+
+    // Define the matrix for position, velociy and acceleration, and it's pointers
+    double r[N][3];
+    double v[N][3];
+    double a[N][3];
+    double m[3] = {1, 1, 1};
+
+    double* r_p = r[0];
+    double* v_p = v[0];
+    double* a_p = a[0];
+    double* m_p = m;
+
+    // Open folder where the initial conditions are stored, and the folder to write the data.
+    FILE *f_init, *f_exp; //pointer to the files
+
+    f_init = fopen("initial_cond.txt", "r"); // open the init_cond file to read
+    f_exp = fopen("data_orbit.txt", "w"); // open the export file to write
+
+    //Read the file and store the data in r and v
+    i = 0;
+    while (fscanf(f_init, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf", &r[i][0], &r[i][1], &r[i][2], &v[i][0], &v[i][1], &v[i][2])!= EOF){
+        i++;
+    }
+    
+    // From those data rescale them
+
+
+    //Calculate the first set of acceleration used for the Verlet algorithm
+    acceleration(r_p, a_p, m_p, N);
+    for (i=0; i<N; i++){
+        printf("%lf, ", a[i][0]);
+    }
+
+/*     for (i = 0; i < t; i+=h){
+        verlet_algorithm(r_p, v_p, a_p, m_p, N, h);
+
+        for (j = 0; j < N; j++){
+            //printf("%lf, %lf, %lf", r[j][0], r[j][1], r[j][2]);
+        }
+
+    } */
+    fclose(f_init);
+    fclose(f_exp);
+
+    return(0);
+}
