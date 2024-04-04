b2f821c252a813bb9aad386597cad4a995681bf2
liuzhuozhuo <liugeorge18@gmail.com>
2024-04-04 15:01:37 +0200

    add geocentric

--- obligatoria1/orbit_func.h
+++ obligatoria1/orbit_func.h
@@ -7,10 +7,11 @@
 //a: matrix (pointer) with the acceleration of all the planets in 3d
 //m: vector (pointer) with the modified mass of the planets
 //N is the total number of planets
-void acceleration (double* r, double* a, double* m, int N) {
+//D: the dimentions of the simulation
+void acceleration (double* r, double* a, double* m, int N, int D) {
     int i, j, k;
     double acc; //Dummy variable to sum all the acceleration for a planet in one component
-    double R[N][N][3]; 
+    double R[N][N][D]; 
     double R_mod[N][N]; //Matrix NxN with the module of the distance between planets
 
     //Calculate the distance vector between planets. 
@@ -18,8 +19,8 @@ void acceleration (double* r, double* a, double* m, int N) {
         for (j = i+1; j < N; j++){
             //Initialize the value to 0, to add recursively the rest of the components
             R_mod[i][j]= 0;
-            for (k = 0; k < 3; k++){
-                R[i][j][k] = *(r+i*3+k) - *(r+j*3+k); 
+            for (k = 0; k < D; k++){
+                R[i][j][k] = *(r+i*D+k) - *(r+j*D+k); 
                 R[j][i][k] = -R[i][j][k];
                 R_mod[i][j] += pow(R[i][j][k], 2);
             }
@@ -30,12 +31,12 @@ void acceleration (double* r, double* a, double* m, int N) {
     //iterate in the planets
      for(i = 0; i < N; i++){
         //iterate in the components
-        for (k = 0; k < 3; k++){
+        for (k = 0; k < D; k++){
             //iterate between planets
-            *(a+i*3+k) = 0;
+            *(a+i*D+k) = 0;
             for (j = 0; j < N; j++){
                 if(i!=j){
-                    *(a+i*3+k) += -(*(m+j))*R[i][j][k]/pow(R_mod[i][j], 3./2.);
+                    *(a+i*D+k) += -(*(m+j))*R[i][j][k]/pow(R_mod[i][j], 3./2.);
                 }
             } 
             
@@ -48,40 +49,51 @@ void acceleration (double* r, double* a, double* m, int N) {
 // v: matrix (pointer) with the velocity of all the planets in 3d
 // h: the step in time for each iteration
 // It returns the values by overwriting the previous vectors
-void verlet_algorithm(double* r, double* v ,double* a, double* m, int N, double h){
+void verlet_algorithm(double* r, double* v ,double* a, double* m, int N, int D, double h){
     int i, j, k;
-    double w[N][3]; // Angular velocity used for the calculation of the new velocities
+    double w[N][D]; // Angular velocity used for the calculation of the new velocities
 
     //Calculate the new positions, and the angular velocity.
     for (i = 0; i < N; i++){
-        for (k = 0; k < 3; k++){
-            *(r+i*3+k) = *(r+i*3+k) + (h*(*(v+i*3+k))) + (h * h * (*(a+i*3+k))/2);
-            w[i][k] = (*(v+i*3+k)) + (h * (*(a+i*3+k))/2);
+        for (k = 0; k < D; k++){
+            *(r+i*D+k) = *(r+i*D+k) + (h*(*(v+i*D+k))) + (h * h * (*(a+i*D+k))/2);
+            w[i][k] = (*(v+i*D+k)) + (h * (*(a+i*D+k))/2);
         }
     }
    //Using the acceleration function calculate the net acceleration for each of the planets
-    acceleration(r, a, m, N);
+    acceleration(r, a, m, N, D);
 
     //Calculate the new velocity of the planets
     for (i = 0; i < N; i++){
-        for (k = 0; k < 3; k++){
-            *(v+i*3+k) = w[i][k] + (h * (*(a+i*3+k))/2);
+        for (k = 0; k < D; k++){
+            *(v+i*D+k) = w[i][k] + (h * (*(a+i*D+k))/2);
         }
     }
 }
 
 // Function that changes the coordinates so the selected object, l,  is in the origin of coordinates
 // Using the same notation as previous functions, 
-void change_coord (double* r, int l,int N){
+// r_exp: the positions with the coordinates changed
+void change_coord (double* r, double* r_exp, int l, int N, int D){
     //Coordinates of the object l
-    double r_l[3] = {*(r+l*N+0), *(r+l*N+1), *(r+l*N+2)};
+    double r_l3[3] = {*(r+l*D+0), *(r+l*D+1), *(r+l*D+2)};
+    double r_l2[3] = {*(r+l*D+0), *(r+l*D+1)};
     int i, k;
 
-    for(i = 0; i < N; i++){
-        for (k = 0; k < N; k++){
-            *(r+i*N+k) -= r_l[k];
+    if (D == 3){
+        for(i = 0; i < N; i++){
+            for (k = 0; k < D; k++){
+                *(r_exp+i*D+k) =  r_l3[k] - *(r+i*D+k);
+            }
+        }
+    }else if (D == 2){
+        for(i = 0; i < N; i++){
+            for (k = 0; k < D; k++){
+                *(r_exp+i*D+k) = r_l2[k] - *(r+i*D+k);
+            }
         }
     }
+    
 }
 
 //Function that calculate the total energy of the system to check if the energy is conserved.
--- obligatoria1/planet_orbits.c
+++ obligatoria1/planet_orbits.c
@@ -10,33 +10,39 @@ int main(){
     double G = 6.67408e-11; //N km2/kg2, Gravitational constant
     double c = 1.4966e11; //km, astronomical units
     double M_s = 1.99e30; //kg, solar mass
-    double t_prime = pow(G*M_s/pow(c, 3.),0.5) ;//
+    double t_prime = pow(G*M_s/pow(c, 3.),0.5) ;// Rescalation constant
     int N = 10; //Number of planets
+    int D = 2; // Dimensions of the simulation
 
     int i, j, k; // Used for the loop
 
     // Define the matrix for position, velociy and acceleration, and it's pointers
-    double r[N][3];
-    double v[N][3];
-    double a[N][3];
+    double r[N][D];
+    double r_geo[N][D];
+    double v[N][D];
+    double a[N][D];
     double m[N];
 
     double* r_p = r[0];
+    double* r_pg = r_geo[0];
     double* v_p = v[0];
     double* a_p = a[0];
     double* m_p = m;
 
     // Open folder where the initial conditions are stored, and the folder to write the data.
-    // The initial condition file has format, where each line --> mass   x   y   z   v_x   v_y   v_z, separated by tab of a different planet
+    // The initial condition file has format, where each line --> mass   x   y   z   v_x   v_y   v_z, 
+    // separated by tab of the different planets
 
-    FILE *f_init, *f_exp; //pointer to the files
+    FILE *f_init, *f_exp, *f_geo; //pointer to the files
 
     f_init = fopen("data/init_cond.txt", "r"); // open the init_cond file to read
     f_exp = fopen("data/planets_data.txt", "w"); // open the export file to write
+    f_geo = fopen("data/geocentric_data.txt", "w"); // open the export file to write
 
     //Read the file and store the data in r and v
     i = 0;
-    while (fscanf(f_init, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf",&m[i] , &r[i][0], &r[i][1], &r[i][2], &v[i][0], &v[i][1], &v[i][2])!= EOF){
+    while (fscanf(f_init, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf", 
+            &m[i] , &r[i][0], &r[i][1], &r[i][2], &v[i][0], &v[i][1], &v[i][2])!= EOF){
         i++;
     }
     
@@ -45,30 +51,31 @@ int main(){
     t = t*t_prime;
     for (i=0; i<N; i++){
         m[i] = m[i]/M_s;
-        
-        printf("%lf,", m[i]);
-        for (k=0; k<3; k++){
+        for (k=0; k<D; k++){
             r[i][k] = r[i][k]/c;
             v[i][k] = v[i][k]/(c*t_prime);
-            printf("%lf, ", r[i][k]);
-            printf("%lf, ", v[i][k]);
         }
-        printf("\n");
     }
 
     //Calculate the first set of acceleration used for the Verlet algorithm
-    acceleration(r_p, a_p, m_p, N);
+    acceleration(r_p, a_p, m_p, N, D);
 
     for (i = 0; i < (int)(t/h); i++){
-        verlet_algorithm(r_p, v_p, a_p, m_p, N, h);
+        verlet_algorithm(r_p, v_p, a_p, m_p, N, D, h);
+        // 
+        change_coord(r_p, r_pg, 3, N, D);
         for (j = 0; j < N; j++){
-            //fprintf(f_exp, "%lf, %lf, %lf \n", r[j][0], r[j][1], r[j][2]);
-            fprintf(f_exp, "%lf, %lf\n", r[j][0], r[j][1]);
+            // Print the positions in the file
+            //fprintf(f_exp, "%lf, %lf, %lf \n", r[j][0], r[j][1], r[j][2]); // For 3D
+            fprintf(f_exp, "%lf, %lf\n", r[j][0], r[j][1]); // For 2D
+            fprintf(f_geo, "%lf, %lf\n", r_geo[j][0], r_geo[j][1]); // For 2D
         }
         fprintf(f_exp, "\n");
+        fprintf(f_geo, "\n");
     }
     fclose(f_init);
     fclose(f_exp);
+    fclose(f_geo);
 
     return(0);
 }
